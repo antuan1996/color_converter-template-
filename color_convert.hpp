ca03e07f67a2862a8fbf196c6b62d08fd7d948ac
Anton Rogazinskii <Rogazinskii@mail.ru>
2015-09-13 00:53:18 +0600

    function's arguments interaction changed

--- color_convert.hpp
+++ color_convert.hpp
@@ -43,72 +43,52 @@ struct Context
 };
 struct Convert_meta
 {
-    int width, height;
-    uint8_t* src_data;
-    uint8_t* dst_data;
-    size_t cur_row;
-    size_t cur_col;
-};
 
-//template < Standart st, bool isInverse> int16_t* set_matrix_koeff();
-//template <Depth src_depth> void read_file(Context &ctx, char* file_name);
-//template <Depth dst_depth> void write_file(Context &ctx, char* file_name);
-//template <Pack data_pack, Colorspace cs> void unpack(Context &ctx);
-//template<Pack data_pack, Colorspace cs> void pack(Context &ctx);
-//template< bool isInverse> void multiple(Context &ctx);
-#include "color_convert.hpp"
-int16_t matrix_bt601_RGB_to_YUV[] =
+    int width, height;
+
+    uint8_t* src_data[3];
+    int src_stride[3];
+
+
+    uint8_t* dst_data[3];
+    int dst_stride[3];
+
+};
+static const int16_t k_matrix_bt601_RGB_to_YUV[] =
 {
     77, 150, 29,
     -44, -87, 131,
     131, -110, -21,
 };
-int16_t matrix_bt601_YUV_to_RGB[] =
+static const int16_t k_matrix_bt601_YUV_to_RGB[] =
 {
-    1, 2, 3,
-    4, 5, 6,
-    7, 8, 9
+    256, 0 , 351,
+    256, -86, -179,
+    256, 444, 0
 };
 
-template < Standart st, bool isInverse>  inline  int16_t* set_matrix_koeff()
+template < Standart st, bool isInverse>  inline  const int16_t* set_matrix_koeff()
 {
     if(isInverse)
     {
         //if(st == BT_709) return matrix_bt601;
         //if(st == BT_2020) return matrix_bt2020;
-        return matrix_bt601_YUV_to_RGB;
+        return k_matrix_bt601_YUV_to_RGB;
     } else
         {
             //if(st == BT_709) return matrix_bt601;
             //if(st == BT_2020) return matrix_bt2020;
-            return matrix_bt601_RGB_to_YUV;
+            return k_matrix_bt601_RGB_to_YUV;
         }
 }
-template <Pack data_pack, Colorspace cs> inline void unpack(const Convert_meta& meta, Context &ctx)
+template <Colorspace cs> inline void unpack(const uint8_t* src_a, const uint8_t* src_b, const uint8_t* src_c, Context &ctx)
 {
     int ch_a, ch_b, ch_c;
     //std::cout << "row is " << ctx.cur_row << " col is" << ctx.cur_col << std::endl;
-    int cur_pos = meta.cur_row * meta.width + meta.cur_col;
     //std::cout << "pos is " << cur_pos << std::endl;
-
-    if ( data_pack == Planar)
-    {
-        //std::cout <<"Planar unpack" << std::endl;
-        int frame_size = meta.width * meta.height;
-            ch_a = meta.src_data[cur_pos];
-            ch_b = meta.src_data[cur_pos + frame_size];
-            ch_c = meta.src_data[cur_pos + 2 * frame_size];
-    } else
-        if ( data_pack == Interleaved)
-        {
-                cur_pos *= 3;
-            //std::cout <<"Interleaved unpack" << std::endl;
-                ch_a = meta.src_data[cur_pos + 0];
-                ch_b = meta.src_data[cur_pos + 1];
-                ch_c = meta.src_data[cur_pos + 2];
-                //std::cout << ch_a[i] << " " << ch_b[i] <<" "<<ch_c[i] << std::endl;
-
-        }
+    ch_a = *src_a;
+    ch_b = *src_b;
+    ch_c = *src_c;
         if( cs == RGB)
         {
             ctx.r = ch_a;
@@ -122,7 +102,7 @@ template <Pack data_pack, Colorspace cs> inline void unpack(const Convert_meta& 
                 }
 }
 
-template<Pack data_pack, Colorspace cs> inline void pack(const Convert_meta& meta  ,Context &ctx)
+template<Pack data_pack, Colorspace cs> inline void pack(uint8_t* dst_a, uint8_t* dst_b, uint8_t* dst_c, Context &ctx)
 {
     int ch_a;
     int ch_b;
@@ -138,18 +118,9 @@ template<Pack data_pack, Colorspace cs> inline void pack(const Convert_meta& met
                     ch_b = ctx.u;
                     ch_c = ctx.v;
                 }
-    if(data_pack  == Planar)
-    {
-        int plane_size = meta.width * meta.height * sizeof(uint8_t);
-        int cur_pos = meta.cur_row * meta.width + meta.cur_col;
-        uint8_t* dst_a = meta.dst_data + cur_pos;
-        uint8_t* dst_b = meta.dst_data + cur_pos + plane_size;
-        uint8_t* dst_c = meta.dst_data + cur_pos + 2 * plane_size;
-
-            *dst_a = ch_a;
-            *dst_b = ch_b;
-            *dst_c = ch_c;
-    }
+    *dst_a = ch_a;
+    *dst_b = ch_b;
+    *dst_c = ch_c;
 }
 
 template< bool isInverse> inline void multiple(Context &ctx, const int16_t* koeff_matrix)
@@ -159,7 +130,7 @@ template< bool isInverse> inline void multiple(Context &ctx, const int16_t* koef
     int src_c = 0;
     int dst_a, dst_b, dst_c;
         if(isInverse){
-            src_a = ctx.y - 16;
+            src_a = ctx.y;
             src_b = ctx.u -128;
             src_c = ctx.v -128;
         } else  {
@@ -168,8 +139,8 @@ template< bool isInverse> inline void multiple(Context &ctx, const int16_t* koef
                     src_c = ctx.b;
                 }
 
-        std::cout << koeff_matrix[0] << " " << koeff_matrix[1] << " " << koeff_matrix[2] << std::endl;
-        std::cout << "****";
+        //std::cout << koeff_matrix[0] << " " << koeff_matrix[1] << " " << koeff_matrix[2] << std::endl;
+        //std::cout << "****";
         dst_a = koeff_matrix[0] * src_a + koeff_matrix[1] * src_b + koeff_matrix[2] * src_c;
         dst_b = koeff_matrix[3] * src_a + koeff_matrix[4] * src_b + koeff_matrix[5] * src_c;
         dst_c = koeff_matrix[6] * src_a + koeff_matrix[7] * src_b + koeff_matrix[8] * src_c;
@@ -193,26 +164,63 @@ template< bool isInverse> inline void multiple(Context &ctx, const int16_t* koef
                 }
 }
 
-template<Colorspace from_cs , Pack from_pack, Colorspace to_cs, Pack to_pack, Standart st> void convert(Convert_meta& meta)
+template<Colorspace from_cs , Pack from_pack, Colorspace to_cs, Pack to_pack, Standart st> void convert(const Convert_meta& meta)
 {
 	Context ctx;
-	int16_t* koeff_matrix;
+	const int16_t* koeff_matrix;
     if( (from_cs == RGB)  && (to_cs == YUV444) )
-        koeff_matrix = (int16_t*) set_matrix_koeff < st, false >();
+        koeff_matrix = set_matrix_koeff < st, false >();
             else
-                koeff_matrix = (int16_t*) set_matrix_koeff < st, true >();
-    for(meta.cur_row = 0; meta.cur_row < meta.height; ++meta.cur_row)
-        for(meta.cur_col = 0; meta.cur_col < meta.width; ++meta.cur_col)
+                koeff_matrix =  set_matrix_koeff < st, true >();
+
+    uint8_t* src_a = meta.src_data[0];
+    uint8_t* src_b = meta.src_data[1];
+    uint8_t* src_c = meta.src_data[2];
+    uint8_t* dst_a = meta.dst_data[0];
+    uint8_t* dst_b = meta.dst_data[1];
+    uint8_t* dst_c = meta.dst_data[2];
+
+    for(int row = 0; row < meta.height; ++row)
+    {
+        for(int col = 0; col < meta.width; ++col)
         {
-            unpack < from_pack ,from_cs >(meta, ctx);
+            unpack <from_cs >( src_a, src_b, src_c, ctx);
 
             if( (from_cs == RGB)  && (to_cs == YUV444) )
                 multiple < false > (ctx, koeff_matrix);
                     else
                         multiple < true > (ctx, koeff_matrix);
 
-            pack < to_pack, to_cs > (meta , ctx);
+            pack < to_pack, to_cs > (dst_a, dst_b, dst_c, ctx);
+
+            if(from_pack == Planar){
+                src_a += 1;
+                src_b += 1;
+                src_c += 1;
+            }
+            if(from_pack == Interleaved){
+                src_a += 3;
+                src_b += 3;
+                src_c += 3;
+            }
+            if(to_pack == Planar){
+                dst_a += 1;
+                dst_b += 1;
+                dst_c += 1;
+            }
+            if(to_pack == Interleaved){
+                dst_a += 3;
+                dst_b += 3;
+                dst_c += 3;
+            }
         }
+        src_a += meta.src_stride[0];
+        src_b += meta.src_stride[1];
+        src_c += meta.src_stride[2];
+        dst_a += meta.dst_stride[0];
+        dst_b += meta.dst_stride[1];
+        dst_c += meta.dst_stride[2];
+    }
 }
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,32 +1,44 @@
 #include "color_convert.hpp"
-
 int main(){
     freopen("report.txt","w",stdout);
     system("pwd");
     Convert_meta test;
     test.width = 176;
     test.height = 144;
-    int frame_size = test.width * test.height * 3;
+    int frame_size = test.width * test.height;
     std::cout << "Getting memory" << std::endl;
-    test.src_data = (uint8_t* )malloc(frame_size);
+
+    // interleaved
+    test.src_data[0] = (uint8_t* )malloc(frame_size * 3);
     if (test.src_data == nullptr) return 1;
-    test.dst_data = (uint8_t* )malloc(frame_size);
+    test.src_data[1] = test.src_data[0] + frame_size;
+    test.src_data[2] = test.src_data[0] + frame_size * 2;
+
+    test.dst_data[0] = (uint8_t* )malloc(frame_size * 3);
     if (test.dst_data == nullptr) return 1;
+    test.dst_data[1] = test.dst_data[0] + frame_size;
+    test.dst_data[2] = test.dst_data[0] + 2 * frame_size;
+
+    for(int i = 0; i < 3; ++i){
+        test.src_stride[i] = 0;
+        test.dst_stride[i] = 0;
+    }
 
     std::cout << "Reading data" << std::endl;
-    FILE* f_in = fopen("line.rgb", "rb");
-    fread(test.src_data, frame_size, 1, f_in);
+    FILE* f_in = fopen("result.yuv", "rb");
+    fread(test.src_data[0], frame_size * 3, 1, f_in);
     fclose(f_in);
+
     std::cout << "I'm ready" << std::endl;
-    convert <RGB, Interleaved, YUV444, Planar, BT_601>(test);
+    convert <YUV444, Planar, RGB, Planar, BT_601>(test);
 
     std::cout << "Writng results" << std::endl;
-    FILE* f_out = fopen("result.yuv", "wb");
-    fwrite(test.dst_data, frame_size, 1, f_out);
+    FILE* f_out = fopen("result.rgb", "wb");
+    fwrite(test.dst_data[0], frame_size * 3, 1, f_out);
     fclose(f_out);
 
     std::cout << "Exit" << std::endl;
-    free(test.src_data);
-    free(test.dst_data);
+    free(test.src_data[0]);
+    free(test.dst_data[0]);
     return 0;
 }
